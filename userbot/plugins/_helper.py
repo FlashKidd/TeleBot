1d7fb37a8d38c2daf29f120b6a4e40f0693d02d0
xditya <58950863+xditya@users.noreply.github.com>
2020-09-27 17:20:41 +0530

    xD

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -7,53 +7,42 @@ from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
+      
+@borg.on(admin_cmd(pattern="help ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    splugin_name = event.pattern_match.group(1)
+    if splugin_name in borg._plugins:
+        s_help_string = borg._plugins[splugin_name].__doc__
+    else:
+        s_help_string = ""
+    _, check_sgnirts = check_data_base_heal_th()
 
-@telebot.on(admin_cmd(pattern="help ?(.*)"))
-async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
-        input_str = event.pattern_match.group(1)
-        if tgbotusername is None or input_str == "text":
-            string = ""
-            for i in CMD_LIST:
-                string += "⚡ " + i + "\n"
-                for iter_list in CMD_LIST[i]:
-                    string += "    `" + str(iter_list) + "`"
-                    string += "\n"
-                string += "\n"
-            if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await bot.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id
-                    )
-                    await event.delete()
-            else:
-                await event.edit(string)
-        elif input_str:
-            if input_str in CMD_LIST:
-                string = "**Commands available in {}** \n\n".format(input_str)
-                for i in CMD_LIST[input_str]:
-                    string += "    " + i
-                    string += "\n"
-                string +="\n**© @TeleBotSupport**"
-                await event.edit(string)
-            else:
-                await event.edit(input_str + " is not a valid plugin!")
-        else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
-            results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername,
-                help_string
-            )
-            await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
-            await event.delete()
+  help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+    borg._iiqsixfourstore[str(event.chat_id)] = {}
+    borg._iiqsixfourstore[
+        str(event.chat_id)
+    ][
+        str(event.id)
+    ] = help_string + "\n\n" + s_help_string
+    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER  # pylint:disable=E0602
+    if tgbotusername is not None:
+        results = await borg.inline_query(  # pylint:disable=E0602
+            tgbotusername,
+            f"TeleBot {event.chat_id} {event.id}"
+        )
+        await results[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
+        )
+    else:
+        await event.reply(
+            help_string + "\n\n" + s_help_string,
+            parse_mode="html"
+        )
+
+    await event.delete()
+
+        
